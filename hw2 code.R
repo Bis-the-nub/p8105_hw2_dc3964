f0d23e5da3972096473f5a55830a46d0f83e660a
Bis-the-nub <jui20010815@gmail.com>
2025-10-01 01:08:20 -0400

    problem 3 almost there, also data update and some minor error debug

--- hw2 code.R
+++ hw2 code.R
@@ -22,13 +22,16 @@ snp =
   separate(date, c("month", "day", "year")) |> 
   select(year, month, everything()) |> 
   select(-day) |> 
-  mutate(month = month.abb[as.integer(month)]) |> 
+  mutate(across(c(month:year), as.integer)) |> 
+  mutate(month = factor(month.name[month], levels = month.name)) |> 
   ## changed format from 2 digit to 4 digit 
   mutate(year = if_else(as.integer(year) <= 15,
                           2000 + as.integer(year),
                           1900 + as.integer(year))) |> 
   ## changed format from integer to character
-  mutate(year = as.character(year))
+  mutate(year = as.character(year)) |> 
+  select(year, month, everything()) |> 
+  arrange(year, month)
 
 ### tidying unemployment
 unemployment =
@@ -39,43 +42,50 @@ unemployment =
 
 ### Merging all datasets
 partial_complete_p1 = left_join(pols_month, snp) 
-complete_data_p1 = left_join(partial_complete, unemployment)
+complete_data_p1 = left_join(partial_complete_p1, unemployment)
 
 
 ## Problem 2
 mr_trashwheel =
-  read_excel("./data/202409 Trash Wheel Collection Data.xlsx",
-             sheet = "Mr. Trash Wheel") |> 
+  read_excel("./data/202509 Trash Wheel Collection Data.xlsx",
+             sheet = "Mr. Trash Wheel",
+             range = "A2:N710",
+             na = c("")
+             ) |> 
   janitor::clean_names() |> 
-  select(-x15,-x16, -dumpster) |> 
-  slice(1:(n() - 2)) |> 
+  filter(!is.na(dumpster)) |> 
   mutate(sports_balls = as.integer(round(sports_balls))) |>
   mutate(wheel_tag = "mr")
 
 professor_trashwheel =
-  read_excel("./data/202409 Trash Wheel Collection Data.xlsx",
-             sheet = "Professor Trash Wheel") |> 
+  read_excel("./data/202509 Trash Wheel Collection Data.xlsx",
+             sheet = "Professor Trash Wheel",
+             range = "A2:M135", 
+             na = c("")
+             ) |> 
   janitor::clean_names() |>
-  select(-dumpster) |> 
-  slice(1:(n() - 3)) |> 
+  filter(!is.na(dumpster)) |> 
   mutate(year = as.character(year)) |> 
   mutate(wheel_tag = "professor")
 
 gwynnda_trashwheel =
-  read_excel("./data/202409 Trash Wheel Collection Data.xlsx",
-             sheet = "Gwynnda Trash Wheel") |> 
+  read_excel("./data/202509 Trash Wheel Collection Data.xlsx",
+             sheet = "Gwynns Falls Trash Wheel",
+             range = "A2:L352",
+             na = c("")
+             ) |> 
   janitor::clean_names() |>
-  select(-dumpster) |> 
-  slice(1:(n() - 1)) |> 
+  filter(!is.na(dumpster)) |> 
   mutate(year = as.character(year)) |> 
   mutate(wheel_tag = "gwynnda")
 
 partial_complete_p2 = full_join(mr_trashwheel, professor_trashwheel)
-complete_data_p2 = full_join(partial_complete_p2, gwynnda_trashwheel)
+complete_data_p2 = full_join(partial_complete_p2, gwynnda_trashwheel) |> 
+  select(wheel_tag, everything())
 
 ### Total weight of trash collected by Professor Trash Wheel
 complete_data_p2 |> 
-  filter(wheel_tag == "mr") |>   
+  filter(wheel_tag == "professor") |>   
   pull(weight_tons) |>                   
   sum()      
 
@@ -96,11 +106,33 @@ zip_zillow =
   pivot_longer(
     cols = matches("^\\d{4}-\\d{1,2}-\\d{1,2}$"), 
     names_to = "date",
-    values_to = "value"
+    values_to = "zori"
   ) |> 
   janitor::clean_names() |> 
   rename(zip_code = region_name)
 
-complete_data_p3 = left_join(zip_zillow, zip_codes) |> 
-  select(zip_code, county_name, date, value, everything())
+complete_data_p3 = left_join(zip_codes, zip_zillow, 
+                             relationship = "many-to-many") |> 
+  select(zip_code, county_name, date, zori, everything())
 
+unique_zips =
+  anti_join(zip_codes, zip_zillow, by = "zip_code")|>
+  arrange(zip_code)
+
+covid_19 =
+  complete_data_p3 |> 
+  select(zip_code, date, zori, county, neighborhood) |> 
+  filter(date %in% c("2020-01-31", "2021-01-31")) |> 
+  pivot_wider(
+    names_from = date,
+    values_from = zori,
+    names_prefix = "zori_"
+ ) |> 
+  mutate(
+    drop = `zori_2020-01-31` - `zori_2021-01-31`
+  ) |> 
+  arrange(desc(drop)) |> 
+  slice(1:10)
+
+knitr::kable(covid_19)
+  
